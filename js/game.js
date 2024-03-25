2bba6076cc0ae35bd55a1db53c45847e70e9d3a5
Дамир <damiriko@yandex.ru>
2024-03-25 14:05:53 +0300

    move

--- js/game.js
+++ js/game.js
@@ -1,6 +1,73 @@
-const canvas = document.getElementById("game"); 
+const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const ground = new Image();
 ground.src = "img/ground.png";
 
+const foodImage = new Image();
+foodImage.src = "img/food.png";
+
+let box = 32;
+let score = 0;
+let dir; //direction, trend
+
+let food = {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 3) * box,
+};
+
+let snake = [];
+snake[0] = {
+    x: 9 * box,
+    y: 10 * box
+};
+
+document.addEventListener("keydown", direction);
+
+function direction(event) {
+    if (event.keyCode == 37 && dir != "right")
+        dir = "left";
+    else if (event.keyCode == 38 && dir != "down")
+        dir = "up";
+    else if (event.keyCode == 39 && dir != "left")
+        dir = "right";
+    else if (event.keyCode == 40 && dir != "up")
+        dir = "down";
+}
+
+
+function drawGame() {
+    ctx.drawImage(ground, 0, 0);
+    ctx.drawImage(foodImage, food.x, food.y);
+
+
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+    ctx.fillStyle = "white";
+    ctx.font = "50px Arial";
+    ctx.fillText(score, box * 2.5, box * 1.7);
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    snake.pop();
+
+    if (dir == "left") snakeX -= box;
+    if (dir == "right") snakeX += box;
+    if (dir == "up") snakeY -= box;
+    if (dir == "down") snakeY += box;
+
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    };
+
+    snake.unshift(newHead);
+
+}
+
+let game = setInterval(drawGame, 100);
